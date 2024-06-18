8ae0bff23cad0d5facd2544339081cf621543544
Dylan Swoope <dylanswoope@Dylans-MacBook-Air.local>
2024-06-18 09:44:38 -0500

    port 80

--- server/index.js
+++ server/index.js
@@ -10,7 +10,7 @@ app.get('/*', (req, res) => {
   res.sendFile(assetFolder + '/index.html');
 });
 
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 80;
 app.listen(port, function(){
   console.log('listening on port '+ port);
 });
